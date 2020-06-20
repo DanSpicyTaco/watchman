e4b65f04962dcb83170ee7d70507f99ce662c6af
Daniel Latimer <latdani@gmail.com>
2020-06-20 10:40:23 +1000

    Changed up some things

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
 from datetime import datetime
+from random import randrange
 import encryption
 import network
 
-network.Network()
+
+def authenticate(encryptor):
+    # Generate a random index between 0 and 100
+    index = random(0, 101)
+
+    # Encrypt the index
+    encrypted_index = encryptor.encrypt(str(index))
+
+    # Send the encrypted index to the Watchman
+
+    # Wait for a value
+
+    # Decrypt the value
+    decrypted_index = encryptor.decrypt(encrypted_index)
+
+    return encrypted_data == decrypted_data
+
+
+# Initialisation - when the UAV is on the ground
+# Create the encryptor
 current_time = datetime.now().isoformat()
 encryptor = encryption.Encryption(time=current_time)
-encrypted_data = encryptor.encrypt("hello")
-decrypted_data = encryptor.decrypt(encrypted_data)
-print(f"Encrypted: {encrypted_data}\nDecrypted: {decrypted_data}")
-
 keys = encryptor.export_keys()
 
+# Set up communication
+network.Network()
+
+# Send the keys to the Watchman
+
 # Do this in the Pi
-encryptor2 = encryption.Encryption(key=keys["key"], IV=keys["IV"])
-encrypted_data = encryptor2.encrypt("hello2")
-decrypted_data = encryptor2.decrypt(encrypted_data)
-print(f"Encrypted2: {encrypted_data}\nDecrypted2: {decrypted_data}")
+# encryptor2 = encryption.Encryption(key=keys["key"], IV=keys["IV"])
+
+# Main loop
+
+# while 1:
+#     if !authenticate(encryptor):
+#         break
+
+# Send an intrusion detection alert
