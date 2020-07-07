109343aafb8ce5d0315595abe2bddb182966d358
Daniel Latimer <latdani@gmail.com>
2020-07-07 11:01:20 +1000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,9 @@
 from setuptools import setup, find_packages
-setup(name='watchman', packages=find_packages())
+
+setup(
+    name='watchman',
+    version='0.0',
+    description='Securing drone communication with an encrypted channel',
+    author='DanSpicyTaco',
+    packages=find_packages()
+)
